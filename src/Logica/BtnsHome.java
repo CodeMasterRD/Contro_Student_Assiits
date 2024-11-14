0259c72dfd2bbcdb9a80adacc84124f04f6fc3db
Israel vargas <126931996+IsraelDev-var@users.noreply.github.com>
2024-11-14 05:58:33 -0400

    interfaces de home y menu terminada con el diseño

---
+++ src/Logica/BtnsHome.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logica;
+
+import GUI.MenuStudentsGUI;
+
+
+public class BtnsHome  {
+    
+    public static void btnEntrar(){
+        System.out.println("ejecutando");
+          MenuStudentsGUI pantallaMenu = new MenuStudentsGUI();
+          pantallaMenu.setVisible(true);
+          pantallaMenu.setLocationRelativeTo(null);
+    }
+    
+    
+}
--- src/Logica/ControStudenAssits.java
+++ src/Logica/ControStudenAssits.java
@@ -1,6 +1,8 @@
 
 package Logica;
 
+import GUI.HomeStudentsGUI;
+import GUI.MenuStudentsGUI;
 import Percistencia.DBConexion;
 import java.io.FileNotFoundException;
 import java.sql.SQLException;
@@ -10,8 +12,11 @@ public class ControStudenAssits {
 
     
     public static void main(String[] args) throws FileNotFoundException, SQLException{
-        // TODO code application logic here
-        
+        //TODO code application logic here
+        HomeStudentsGUI pantallaHome = new HomeStudentsGUI();
+        pantallaHome.setVisible(true);
+        pantallaHome.setLocationRelativeTo(null);
+   
         DBConexion conexion = new DBConexion();
         conexion.getConexion();
     }
